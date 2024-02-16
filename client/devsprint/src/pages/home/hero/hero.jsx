56c610fa2433387b96867650b37ec5910c86db09
Abrar Mahir Esam <87098848+Abrar118@users.noreply.github.com>
2024-02-17 01:37:46 +0600

    with part of hero section

---
+++ client/devsprint/src/pages/home/hero/hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./hero.css";
+
+const Hero = () => {
+  return <div className="hero-section">
+   <div className="hero-title">
+      <h1>Introduce Your Product Quickly & Effectively</h1>
+      <p>Get your project done in a sprint</p>
+   </div>
+  </div>;
+};
+
+export default Hero;
