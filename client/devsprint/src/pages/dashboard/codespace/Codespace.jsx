547e5d58b8b0afb0c563df82c18498d5838d9358
Abrar Mahir Esam <87098848+Abrar118@users.noreply.github.com>
2024-02-19 23:08:29 +0600

    codespace done

---
+++ client/devsprint/src/pages/dashboard/codespace/Codespace.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useRef } from "react";
+import "./Codespace.css";
+import { motion } from "framer-motion";
+import Editor from "@monaco-editor/react";
+import { Doc } from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import { MonacoBinding } from "y-monaco";
+
+const Codespace = () => {
+  const editorRef = useRef(null);
+  const user = JSON.parse(
+    localStorage.getItem("token") || sessionStorage.getItem("token")
+  ).email;
+
+  const handleMount = (editor, monaco) => {
+    editorRef.current = editor;
+
+    const document = new Doc();
+    const provider = new WebrtcProvider("code-room", document, {
+      signaling: ["ws://localhost:5173"],
+    });
+    const type = document.getText("monaco");
+    const yBinding = new MonacoBinding(
+      type,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    );
+
+    yBinding.observe((event) => {
+      console.log(event);
+    });
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      className="editor-container"
+    >
+      <Editor
+        height={"100%"}
+        width={"100%"}
+        defaultLanguage="javascript"
+        defaultValue="//Start coding here..."
+        theme="vs-dark"
+        options={{
+          fontSize: 16,
+          minimap: {
+            enabled: false,
+          },
+        }}
+        onMount={handleMount}
+      />
+    </motion.div>
+  );
+};
+
+export default Codespace;
